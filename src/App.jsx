3953abe58806b1f2a564af1c76f184bfd17c965a
Royal Finch <Royal2791@gmail.com>
2024-03-07 14:23:09 -0500

    Add state for score and best score.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Header from './components/Header';
 import Game from './components/Game';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
   return (
     <div className="app-container">
       <Header />
