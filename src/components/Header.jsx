56a9443dd8ee4b0187a93a1ff87aad4ee5d7c343
Royal Finch <Royal2791@gmail.com>
2024-03-09 00:52:48 -0500

    add logo and style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import '../css/Header.css';
+import logoImg from '../assets/LoL_Logo.png';
 function Header({ score, bestScore }) {
   return (
     <header>
-      <h1>League of Legends Memory Game</h1>
+      <img src={logoImg} alt="LoL Logo" />
+      <h1>Memory Game</h1>
       <h3>Score: {score}</h3>
       <h3>Best Score: {bestScore}</h3>
     </header>
